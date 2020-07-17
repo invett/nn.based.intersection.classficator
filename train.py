b93655e5a31a70e3e1ae5f6e9e3ed7be9f454062
Alvaro <alvaro.hernandezsaz@uah.es>
2020-07-17 11:48:08 +0200

    Minor changes in training loop

--- train.py
+++ train.py
@@ -13,6 +13,7 @@ from dataloaders.sequencedataloader import SequenceDataset
 from model.resnet_models import get_model_resnet, get_model_resnext
 from sklearn.model_selection import KFold
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+from tensorboardX import SummaryWriter
 
 
 def validation(args, model, criterion, dataloader_val):
@@ -43,17 +44,21 @@ def validation(args, model, criterion, dataloader_val):
         predlist = np.append(predlist, predict)
 
     loss_val_mean = loss_record / len(dataloader_val)
-    print('loss for validation : %f' % loss_val_mean)
+    print('loss for validation : %f\n' % loss_val_mean)
 
     # Calculate validation metrics
     conf_matrix = confusion_matrix(labellist, predlist)
     report_dict = classification_report(labellist, predlist, target_names=target_names, output_dict=True,
                                         zero_division=0)
     acc = accuracy_score(labellist, predlist)
-    return report_dict, conf_matrix, acc
+    return report_dict, conf_matrix, acc, loss_val_mean
 
 
 def train(args, model, optimizer, dataloader_train, dataloader_val, acc_pre):
+
+    writer = SummaryWriter()
+    step = 0
+
     if not os.path.isdir(args.save_model_path):
         os.mkdir(args.save_model_path)
 
@@ -89,14 +94,18 @@ def train(args, model, optimizer, dataloader_train, dataloader_val, acc_pre):
             tq.set_postfix(loss='%.6f' % loss)
 
             loss_record += loss.item()
+            writer.add_scalar('Train/loss_step', loss.item(), step)
+            step += 1
 
         tq.close()
         loss_train_mean = loss_record / len(dataloader_train)
+        writer.add_scalar('Train/loss_epoch', loss_train_mean, epoch)
         print('loss for train : %f' % loss_train_mean)
 
         if epoch % args.validation_step == 0:
-            report, confusion_matrix, acc = validation(args, model, criterion, dataloader_val)
-
+            report, confusion_matrix, acc, val_loss = validation(args, model, criterion, dataloader_val)
+            writer.add_scalar('Val/loss', val_loss, epoch)
+            writer.add_scalar('Val/acc', acc, epoch)
             if kfold_acc < acc:
                 patience = 0
                 kfold_acc = acc
@@ -106,7 +115,7 @@ def train(args, model, optimizer, dataloader_train, dataloader_val, acc_pre):
                     print('Best global accuracy: {}'.format(kfold_acc))
                     print('Saving model: ', os.path.join(args.save_model_path, 'model_{}.pth'.format(args.resnetmodel)))
                     torch.save(bestModel, os.path.join(args.save_model_path, 'model_{}.pth'.format(args.resnetmodel)))
-                    print('Saving report and confusion matrix')
+                    print('Saving report and confusion matrix\n')
                     df_report = pd.DataFrame.from_dict(report)
                     df_report = df_report.transpose()
                     df_report.to_csv('report.csv', sep=';')
@@ -124,6 +133,8 @@ def train(args, model, optimizer, dataloader_train, dataloader_val, acc_pre):
 
         if patience >= args.patience > 0:
             break
+
+    writer.close()
 
     with open('acc_log.txt', 'a') as logfile:
         logfile.write('Acc: {}\n'.format(kfold_acc))
@@ -151,6 +162,9 @@ def main(args):
     for train_index, test_index in kf.split(list(range(len(dataset)))):
         train_data = torch.utils.data.Subset(dataset, train_index)
         test_data = torch.utils.data.Subset(dataset, test_index)
+
+        print('Train data size: {}'.format(len(train_data)))
+        print('Test data size: {}\n'.format(len(test_data)))
 
         dataloader_train = DataLoader(train_data, batch_size=args.batch_size, shuffle=True,
                                       num_workers=args.num_workers)
@@ -196,8 +210,8 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', type=int, default=4, help='Number of images in each batch')
     parser.add_argument('--resnetmodel', type=str, default="resnet18",
                         help='The context path model you are using, resnet18, resnet50 or resnet101.')
-    parser.add_argument('--learning_rate', type=float, default=0.01, help='learning rate used for train')
-    parser.add_argument('--momentum', type=float, default=0.99, help='momentum used for train')
+    parser.add_argument('--learning_rate', type=float, default=0.001, help='learning rate used for train')
+    parser.add_argument('--momentum', type=float, default=0.9, help='momentum used for train')
 
     parser.add_argument('--num_workers', type=int, default=4, help='num of workers')
     parser.add_argument('--num_classes', type=int, default=7, help='num of object classes')
