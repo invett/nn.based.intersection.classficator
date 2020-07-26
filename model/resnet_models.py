066a94652286dd4d111d7888a3cfdc262c8863cb
Alvaro <alvaro.hernandezsaz@uah.es>
2020-07-26 11:45:46 +0200

    Complex classifier implementation

--- model/resnet_models.py
+++ model/resnet_models.py
@@ -1,6 +1,36 @@
 import torch
 from torchvision import models
 
+from torch import nn
+
+"""
+2048 > 1024
+2048 > 1024
+1024 > 512
+512 > 256
+256 > num_classes
+"""
+
+
+def set_classifier(num_class):
+    classifier = nn.Sequential(
+        nn.Linear(2048, 1024),
+        nn.BatchNorm1d(1024),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(1024, 512),
+        nn.BatchNorm1d(512),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(512, 256),
+        nn.BatchNorm1d(256),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(256, num_classes)
+    )
+
+    return classifier
+
 
 def get_model_resnet(model_version, num_classes, transfer):
     if model_version == 'resnet18':
@@ -20,19 +50,22 @@ def get_model_resnet(model_version, num_classes, transfer):
         if transfer:
             for param in model.parameters():
                 param.requires_grad = False
-        model.fc = torch.nn.Linear(2048, num_classes)
+        fc = set_classifier(num_classes)
+        model.fc = fc
     elif model_version == 'resnet101':
         model = models.resnet34(pretrained=True)
         if transfer:
             for param in model.parameters():
                 param.requires_grad = False
-        model.fc = torch.nn.Linear(2048, num_classes)
+        fc = set_classifier(num_classes)
+        model.fc = fc
     elif model_version == 'resnet152':
         model = models.resnet34(pretrained=True)
         if transfer:
             for param in model.parameters():
                 param.requires_grad = False
-        model.fc = torch.nn.Linear(2048, num_classes)
+        fc = set_classifier(num_classes)
+        model.fc = fc
 
     return model
 
