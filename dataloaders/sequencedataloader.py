88f0b06196e206eddc8d401f2ba5a89d75ab0e49
Augusto Luis Ballardini <augusto.ballardini@uah.es>
2020-09-28 16:57:36 +0200

    1. extra workaround for opencv ... hostname check 2. now we can set the seed
    Signed-off-by: Augusto Luis Ballardini <augusto.ballardini@uah.es>

--- dataloaders/sequencedataloader.py
+++ dataloaders/sequencedataloader.py
@@ -772,6 +772,10 @@ class teacher_tripletloss_generated(Dataset):
 
         """
 
+        # safe to delete, this is for the SEED test
+        if idx == 0:
+            print("Random seed to check: " + str(np.random.rand() ))
+
         # identify the typology for anchor item
         anchor_type = self.samples[idx][0]
 
