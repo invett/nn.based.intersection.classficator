7efb84b99397c52883e783985e7adc1071048bfa
Augusto Luis Ballardini <augusto.ballardini@uah.es>
2020-10-02 01:26:53 +0200

    decimateStep not decimate!! :'(
    Signed-off-by: Augusto Luis Ballardini <augusto.ballardini@uah.es>

--- train.py
+++ train.py
@@ -387,10 +387,10 @@ def main(args, model=None):
             elif args.dataloader == "generatedDataset":
                 val_dataset = fromGeneratedDataset(val_path, args.distance, transform=generateTransforms,
                                                    loadlist=False,
-                                                   decimate=args.decimate)  # todo fix loadlist for k-fold
+                                                   decimateStep=args.decimate)  # todo fix loadlist for k-fold
                 train_dataset = fromGeneratedDataset(train_path, args.distance, transform=generateTransforms,
                                                      loadlist=False,
-                                                     decimate=args.decimate)  # todo fix loadlist for k-fold
+                                                     decimateStep=args.decimate)  # todo fix loadlist for k-fold
 
             elif args.dataloader == "triplet_OBB":
 
