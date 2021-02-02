5f6ec6378cd78e92d7c1d744c7b75aca50d4a2ea
Augusto Luis Ballardini <augusto.ballardini@uah.es>
2021-02-02 15:02:13 +0100

    changes to test the new dataloader alcala
    Signed-off-by: Augusto Luis Ballardini <augusto.ballardini@uah.es>

--- scripts/checkSequencesDataloader.py
+++ scripts/checkSequencesDataloader.py
@@ -11,11 +11,12 @@
 import os
 import numpy as np
 
-from dataloaders.sequencedataloader import SequencesDataloader
+from dataloaders.sequencedataloader import SequencesDataloader, alcala26012021
 from torch.utils.data import DataLoader
 
 data_path = '/home/ballardini/Desktop/ALCALA/R1_video_0002_camera1_png/'
 data_path = ['/media/ballardini/7D3AD71E1EACC626/ALVARO/Secuencias/2011_10_03_drive_0027_sync/']
+data_path = '/home/ballardini/Desktop/alcala-26.01.2021/'
 
 # All sequence folders
 # folders = np.array([os.path.join(data_path, folder) for folder in os.listdir(data_path) if
@@ -24,8 +25,10 @@ data_path = ['/media/ballardini/7D3AD71E1EACC626/ALVARO/Secuencias/2011_10_03_dr
 #dataset = SequencesDataloader(root='/media/ballardini/7D3AD71E1EACC626/ALVARO/Secuencias/',
 #                              folders=['2011_10_03_drive_0027_sync'])
 
-dataset = SequencesDataloader(root='/home/ballardini/Desktop/ALCALA/',
-                              folders=['R2_video_0002_camera1_png'])
+# dataset = SequencesDataloader(root='/home/ballardini/Desktop/ALCALA/',
+#                               folders=['R2_video_0002_camera1_png'])
+
+dataset = alcala26012021(path_filename='/home/ballardini/Desktop/alcala-26.01.2021/train_list.txt')
 
 loader = DataLoader(
     dataset,
