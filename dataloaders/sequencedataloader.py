e3cd30f9c5e8b048a87b936322dc46c2de80cf08
Alvaro <alvaro.hernandezsaz@uah.es>
2021-02-07 11:18:11 +0100

    dataloader update ***

--- dataloaders/sequencedataloader.py
+++ dataloaders/sequencedataloader.py
@@ -66,8 +66,6 @@ class alcala26012021(Dataset):
         imagepath = self.images[idx]
         image = Image.open(imagepath)
 
-        print(imagepath)
-
         label = int(self.labels[idx])
         neg_label = choice([i for i in range(0, 7) if i != label])
 
