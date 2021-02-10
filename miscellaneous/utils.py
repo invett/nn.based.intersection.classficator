815796d7bb75400421021adebffca635a04e7ccc
Augusto Luis Ballardini <augusto.ballardini@uah.es>
2021-02-10 16:25:26 +0100

    adding routine to parse booleans with argparse ...
    Signed-off-by: Augusto Luis Ballardini <augusto.ballardini@uah.es>

--- miscellaneous/utils.py
+++ miscellaneous/utils.py
@@ -747,3 +747,24 @@ def acc_triplet_score(args, out_anchor, out_positive, out_negative):
         acc = torch.sum(distance_pos < distance_neg) / args.batch_size
 
     return acc
+
+
+def str2bool(v):
+    """
+    Parsing boolean values with argparse
+    https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
+
+    Args:
+        v:
+
+    Returns:
+
+    """
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
