d4c910f82eb5f1ec5298712f63938f0591e2be0f
Alvaro <alvaro.hernandezsaz@uah.es>
2021-02-14 18:51:05 +0100

    hardcode kitti360 division

--- train.py
+++ train.py
@@ -728,7 +728,7 @@ def main(args, model=None):
         # ALCALA
         if args.dataset_val is None:
             val_path = os.path.join(args.dataset, 'validation/2nd.split.validation_list.txt')
-            train_path = os.path.join(args.dataset, 'train/2nd.split.train_list.txt')
+            train_path = os.path.join(args.dataset, 'train/train_list_2.txt')
             test_path = os.path.join(args.dataset, 'test/2nd.split.test_list.txt')
         else:
             train_path = os.path.join(args.dataset, 'test/test_list.txt')  # DATASET ALCALA 12_02_21
