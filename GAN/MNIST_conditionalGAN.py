d66ae52012d84fbce4ff0a3063f77d8b913b9372
Augusto Luis Ballardini <augusto.ballardini@uah.es>
2021-03-02 12:34:55 +0100

    adding base GAN test
    Signed-off-by: Augusto Luis Ballardini <augusto.ballardini@uah.es>

---
+++ GAN/MNIST_conditionalGAN.py
@@ -0,0 +1,287 @@
+#!/usr/bin/env python
+
+import torch
+from torch import nn
+from tqdm.auto import tqdm
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torchvision.utils import make_grid
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+import math
+
+torch.manual_seed(0)
+
+def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28), nrow=5, show=True):
+    '''
+    Function for visualizing images: Given a tensor of images, number of images, and
+    size per image, plots and prints the images in an uniform grid.
+    '''
+    image_tensor = (image_tensor + 1) / 2
+    image_unflat = image_tensor.detach().cpu()
+    image_grid = make_grid(image_unflat[:num_images], nrow=nrow)
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    if show:
+        plt.show()
+
+
+
+class Generator(nn.Module):
+    '''
+    Generator Class
+    '''
+    def __init__(self, input_dim=10, im_chan=1, hidden_dim=64):
+        super(Generator, self).__init__()
+        self.input_dim = input_dim
+        # Build the neural network
+        self.gen = nn.Sequential(
+            self.make_gen_block(input_dim, hidden_dim * 4),
+            self.make_gen_block(hidden_dim * 4, hidden_dim * 2, kernel_size=4, stride=1),
+            self.make_gen_block(hidden_dim * 2, hidden_dim),
+            self.make_gen_block(hidden_dim, im_chan, kernel_size=4, final_layer=True),
+        )
+
+    def make_gen_block(self, input_channels, output_channels, kernel_size=3, stride=2, final_layer=False):
+        '''
+        Function to return a sequence of operations corresponding to a generator block of DCGAN;
+        a transposed convolution, a batchnorm (except in the final layer), and an activation.
+        Parameters:
+            input_channels: how many channels the input feature representation has
+            output_channels: how many channels the output feature representation should have
+            kernel_size: the size of each convolutional filter, equivalent to (kernel_size, kernel_size)
+            stride: the stride of the convolution
+            final_layer: a boolean, true if it is the final layer and false otherwise 
+                      (affects activation and batchnorm)
+        '''
+        if not final_layer:
+            return nn.Sequential(
+                nn.ConvTranspose2d(input_channels, output_channels, kernel_size, stride),
+                nn.BatchNorm2d(output_channels),
+                nn.ReLU(inplace=True),
+            )
+        else:
+            return nn.Sequential(
+                nn.ConvTranspose2d(input_channels, output_channels, kernel_size, stride),
+                nn.Tanh(),
+            )
+
+    def forward(self, noise):
+        '''
+        Function for completing a forward pass of the generator: Given a noise tensor, 
+        returns generated images.
+        Parameters:
+            noise: a noise tensor with dimensions (n_samples, input_dim)
+        '''
+        x = noise.view(len(noise), self.input_dim, 1, 1)
+        return self.gen(x)
+
+def get_noise(n_samples, input_dim, device='cpu'):
+    '''
+    Function for creating noise vectors: Given the dimensions (n_samples, input_dim)
+    creates a tensor of that shape filled with random numbers from the normal distribution.
+    Parameters:
+        n_samples: the number of samples to generate, a scalar
+        input_dim: the dimension of the input vector, a scalar
+        device: the device type
+    '''
+    return torch.randn(n_samples, input_dim, device=device)
+
+
+
+class Discriminator(nn.Module):
+    '''
+    Discriminator Class
+    '''
+    def __init__(self, im_chan=1, hidden_dim=64):
+        super(Discriminator, self).__init__()
+        self.disc = nn.Sequential(
+            self.make_disc_block(im_chan, hidden_dim),
+            self.make_disc_block(hidden_dim, hidden_dim * 2),
+            self.make_disc_block(hidden_dim * 2, 1, final_layer=True),
+        )
+
+    def make_disc_block(self, input_channels, output_channels, kernel_size=4, stride=2, final_layer=False):
+        '''
+        Function to return a sequence of operations corresponding to a discriminator block of the DCGAN; 
+        a convolution, a batchnorm (except in the final layer), and an activation (except in the final layer).
+        Parameters:
+            input_channels: how many channels the input feature representation has
+            output_channels: how many channels the output feature representation should have
+            kernel_size: the size of each convolutional filter, equivalent to (kernel_size, kernel_size)
+            stride: the stride of the convolution
+            final_layer: a boolean, true if it is the final layer and false otherwise 
+                      (affects activation and batchnorm)
+        '''
+        if not final_layer:
+            return nn.Sequential(
+                nn.Conv2d(input_channels, output_channels, kernel_size, stride),
+                nn.BatchNorm2d(output_channels),
+                nn.LeakyReLU(0.2, inplace=True),
+            )
+        else:
+            return nn.Sequential(
+                nn.Conv2d(input_channels, output_channels, kernel_size, stride),
+            )
+
+    def forward(self, image):
+        '''
+        Function for completing a forward pass of the discriminator: Given an image tensor, 
+        returns a 1-dimension tensor representing fake/real.
+        Parameters:
+            image: a flattened image tensor with dimension (im_chan)
+        '''
+        disc_pred = self.disc(image)
+        return disc_pred.view(len(disc_pred), -1)
+
+
+def get_one_hot_labels(labels, n_classes):
+    '''
+    Function for creating one-hot vectors for the labels.
+    Parameters:
+        labels: tensor of labels from the dataloader
+        n_classes: the total number of classes in the dataset
+    '''
+    return torch.nn.functional.one_hot(labels, n_classes)
+
+
+
+def combine_vectors(x, y):
+    combined = (torch.cat((x.float(),y.float()),1))
+    return combined
+
+
+mnist_shape = (1, 28, 28)
+n_classes = 10
+criterion = nn.BCEWithLogitsLoss()
+n_epochs = 200
+z_dim = 64
+display_step = 500
+batch_size = 128
+lr = 0.0002
+device = 'cuda'
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,)),
+])
+
+dataloader = DataLoader(
+    MNIST('.', download=False, transform=transform),
+    batch_size=batch_size,
+    shuffle=True)
+
+
+def get_input_dimensions(z_dim, mnist_shape, n_classes):
+    '''
+    Function for getting the size of the conditional input dimensions 
+    from z_dim, the image shape, and number of classes.
+    Parameters:
+        z_dim: the dimension of the noise vector, a scalar
+        mnist_shape: the shape of each MNIST image as (C, W, H), which is (1, 28, 28)
+        n_classes: the total number of classes in the dataset
+    Returns: 
+        generator_input_dim: the input dimensionality of the conditional generator, 
+                          which takes the noise and class vectors
+        discriminator_im_chan: the number of input channels to the discriminator
+    '''
+    generator_input_dim = z_dim+n_classes
+    discriminator_im_chan = mnist_shape[0]+n_classes
+    return generator_input_dim, discriminator_im_chan
+
+
+generator_input_dim, discriminator_im_chan = get_input_dimensions(z_dim, mnist_shape, n_classes)
+
+gen = Generator(input_dim=generator_input_dim).to(device)
+gen_opt = torch.optim.Adam(gen.parameters(), lr=lr)
+disc = Discriminator(im_chan=discriminator_im_chan).to(device)
+disc_opt = torch.optim.Adam(disc.parameters(), lr=lr)
+
+def weights_init(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+    if isinstance(m, nn.BatchNorm2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+        torch.nn.init.constant_(m.bias, 0)
+        
+gen = gen.apply(weights_init)
+disc = disc.apply(weights_init)
+cur_step = 0
+generator_losses = []
+discriminator_losses = []
+noise_and_labels = False
+fake = False
+fake_image_and_labels = False
+real_image_and_labels = False
+disc_fake_pred = False
+disc_real_pred = False
+
+for epoch in range(n_epochs):
+    for real, labels in tqdm(dataloader):
+        cur_batch_size = len(real)
+        # Flatten the batch of real images from the dataset
+        real = real.to(device)
+
+        one_hot_labels = get_one_hot_labels(labels.to(device), n_classes)
+        image_one_hot_labels = one_hot_labels[:, :, None, None]
+        image_one_hot_labels = image_one_hot_labels.repeat(1, 1, mnist_shape[1], mnist_shape[2])
+
+        ### Update discriminator ###
+        # Zero out the discriminator gradients
+        disc_opt.zero_grad()
+        # Get noise corresponding to the current batch_size 
+        fake_noise = get_noise(cur_batch_size, z_dim, device=device)
+        noise_and_labels = combine_vectors(fake_noise, one_hot_labels)
+        fake = gen(noise_and_labels)
+        
+        fake_image_and_labels = combine_vectors(fake,image_one_hot_labels)
+        real_image_and_labels = combine_vectors(real, image_one_hot_labels)
+        disc_fake_pred = disc(fake_image_and_labels)
+        disc_real_pred = disc(real_image_and_labels)
+        
+        disc_fake_loss = criterion(disc_fake_pred, torch.zeros_like(disc_fake_pred))
+        disc_real_loss = criterion(disc_real_pred, torch.ones_like(disc_real_pred))
+        disc_loss = (disc_fake_loss + disc_real_loss) / 2
+        disc_loss.backward(retain_graph=True)
+        disc_opt.step() 
+
+        # Keep track of the average discriminator loss
+        discriminator_losses += [disc_loss.item()]
+
+        ### Update generator ###
+        # Zero out the generator gradients
+        gen_opt.zero_grad()
+
+        fake_image_and_labels = combine_vectors(fake, image_one_hot_labels)
+        disc_fake_pred = disc(fake_image_and_labels)
+        gen_loss = criterion(disc_fake_pred, torch.ones_like(disc_fake_pred))
+        gen_loss.backward()
+        gen_opt.step()
+
+        # Keep track of the generator losses
+        generator_losses += [gen_loss.item()]
+        
+        if cur_step % display_step == 0 and cur_step > 0:
+            gen_mean = sum(generator_losses[-display_step:]) / display_step
+            disc_mean = sum(discriminator_losses[-display_step:]) / display_step
+            print(f"Step {cur_step}: Generator loss: {gen_mean}, discriminator loss: {disc_mean}")
+            show_tensor_images(fake)
+            show_tensor_images(real)
+            step_bins = 20
+            x_axis = sorted([i * step_bins for i in range(len(generator_losses) // step_bins)] * step_bins)
+            num_examples = (len(generator_losses) // step_bins) * step_bins
+            plt.plot(
+                range(num_examples // step_bins), 
+                torch.Tensor(generator_losses[:num_examples]).view(-1, step_bins).mean(1),
+                label="Generator Loss"
+            )
+            plt.plot(
+                range(num_examples // step_bins), 
+                torch.Tensor(discriminator_losses[:num_examples]).view(-1, step_bins).mean(1),
+                label="Discriminator Loss"
+            )
+            plt.legend()
+            plt.show()
+            
+        cur_step += 1
+
