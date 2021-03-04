f0710a9497dedb7407216d654680f2601c575c45
Sandra <58729912+sandracl72@users.noreply.github.com>
2021-03-04 21:19:19 +0100

    Update WGAN-GP_pylightning.py

--- GAN/WGAN-GP_pylightning.py
+++ GAN/WGAN-GP_pylightning.py
@@ -1,5 +1,4 @@
 """
-To run this template just do:
 python wgan_gp.py
 After a few epochs, launch TensorBoard to see the images being generated at every batch:
 tensorboard --logdir default
@@ -248,7 +247,7 @@ class WGANGP(LightningModule):
             transforms.ToTensor(),
             transforms.Normalize([0.5], [0.5]),
         ])
-        dataset = MNIST(os.getcwd(), train=True, download=True, transform=transform)
+        dataset = MNIST(os.getcwd(), train=True, download=True, transform=transform)  
         return DataLoader(dataset, batch_size=self.batch_size)
 
     def on_epoch_end(self):
@@ -281,8 +280,8 @@ def main(args: Namespace) -> None:
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument("--gpus", type=int, default=0, help="number of GPUs")
-    parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
+    parser.add_argument("--gpus", type=int, default=1, help="number of GPUs")
+    parser.add_argument("--batch_size", type=int, default=128, help="size of the batches")
     parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
     parser.add_argument("--b1", type=float, default=0.5,
                         help="adam: decay of first order momentum of gradient")
