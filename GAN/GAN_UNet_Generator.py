878ad45f813855ef2d17dcd51209b624726cdf0a
Sandra <58729912+sandracl72@users.noreply.github.com>
2021-03-09 22:16:45 +0100

    Fix Generator padding

--- GAN/GAN_UNet_Generator.py
+++ GAN/GAN_UNet_Generator.py
@@ -70,7 +70,7 @@ class Generator(nn.Module):
         self.gen = nn.Sequential(ExpandingBlock(input_dim, hidden_dim * 8, kernel_1=1, kernel_2=1, pad_1=1, pad_2=1),
                                  ExpandingBlock(hidden_dim * 8,  hidden_dim * 8, kernel_1=1, kernel_2=1, pad_1=1, pad_2=1),
                                  ExpandingBlock(hidden_dim * 8,  hidden_dim * 8, kernel_1=2, kernel_2=3, pad_1=0, pad_2=0),
-                                 ExpandingBlock(hidden_dim * 8,  hidden_dim * 4, kernel_1=2, kernel_2=3, pad_1=0, pad_2=0),
+                                 ExpandingBlock(hidden_dim * 8,  hidden_dim * 4, kernel_1=2, kernel_2=3, pad_1=1, pad_2=0),
                                  ExpandingBlock(hidden_dim * 4,  hidden_dim * 2, kernel_1=2, kernel_2=3, pad_1=1, pad_2=1),
                                  ExpandingBlock(hidden_dim * 2,  hidden_dim, kernel_1=2, kernel_2=3, pad_1=0, pad_2=1),
                                  #ExpandingBlock(input_dim, im_chan, kernel_1=2, kernel_2=3, pad_1=0, pad_2=1),
