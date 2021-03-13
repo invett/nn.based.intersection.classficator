3c88879b3d8fe1709779d0eb68d49b7a8557d6ca
Sandra <58729912+sandracl72@users.noreply.github.com>
2021-03-13 16:08:08 +0100

    Small Fix

--- GAN/GAN_UNet_Generator.py
+++ GAN/GAN_UNet_Generator.py
@@ -343,7 +343,7 @@ class WGANGP(LightningModule):
                 exit(-1)
 
             rgb_image_test_transforms = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor(),
-                                                            transforms.Normalize((0.485, 0.456, 0.406),
+                                                            transforms.Normalize((0.485, 0.456, 0.406)),
                                                             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
             dataset_ = txt_dataloader(train_path, transform=rgb_image_test_transforms, decimateStep=self.decimate)
